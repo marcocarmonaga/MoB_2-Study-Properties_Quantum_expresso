a239c154c6724914406b1175152ae528067b8ae3
Marco Antonio Carmona <marcoa.carmonagalvan@viep.com.mx>
2022-05-28 22:43:49 -0500

    creando calculo de interfase y fonones

---
+++ notebooks-scripts/28-ejecutando-archivos-interfase-fonones.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ../data/processed/calculo-de-interfase-y-fonones
+
+for directory in $(ls)
+    do
+        cd $directory
+        mpirun -np 8 pw.x -in mob2_interfase.in > mob2_interfase.out
+        mpirun -np 8 pw.x -in mob2_fonon_01.in > mob2_fonon_01.out
+        mpirun -np 8 pw.x -in mob2_fonon_02.in > mob2_fonon_02.out
+        mpirun -np 8 pw.x -in mob2_fonon_03.in > mob2_fonon_03.out
+        mpirun -np 8 pw.x -in mob2_fonon_04.in > mob2_fonon_04.out
+        mpirun -np 8 pw.x -in mob2_fonon_05.in > mob2_fonon_05.out
+        mpirun -np 8 pw.x -in mob2_fonon_06.in > mob2_fonon_06.out
+        echo Terminado $directory
+        cd ..
+
+shutdown now
