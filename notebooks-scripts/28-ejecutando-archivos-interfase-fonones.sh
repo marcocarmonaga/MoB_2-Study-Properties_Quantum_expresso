ba37387de60bbfbc9c3a3aefbc508a9eaa155df9
Marco Antonio Carmona <marcoa.carmonagalvan@viep.com.mx>
2022-05-28 22:50:18 -0500

    new files

--- notebooks-scripts/28-ejecutando-archivos-interfase-fonones.sh
+++ notebooks-scripts/28-ejecutando-archivos-interfase-fonones.sh
@@ -5,13 +5,14 @@ for directory in $(ls)
     do
         cd $directory
         mpirun -np 8 pw.x -in mob2_interfase.in > mob2_interfase.out
-        mpirun -np 8 pw.x -in mob2_fonon_01.in > mob2_fonon_01.out
-        mpirun -np 8 pw.x -in mob2_fonon_02.in > mob2_fonon_02.out
-        mpirun -np 8 pw.x -in mob2_fonon_03.in > mob2_fonon_03.out
-        mpirun -np 8 pw.x -in mob2_fonon_04.in > mob2_fonon_04.out
-        mpirun -np 8 pw.x -in mob2_fonon_05.in > mob2_fonon_05.out
-        mpirun -np 8 pw.x -in mob2_fonon_06.in > mob2_fonon_06.out
+        mpirun -np 8 ph.x -in mob2_fonon_01.in > mob2_fonon_01.out
+        mpirun -np 8 ph.x -in mob2_fonon_02.in > mob2_fonon_02.out
+        mpirun -np 8 ph.x -in mob2_fonon_03.in > mob2_fonon_03.out
+        mpirun -np 8 ph.x -in mob2_fonon_04.in > mob2_fonon_04.out
+        mpirun -np 8 ph.x -in mob2_fonon_05.in > mob2_fonon_05.out
+        mpirun -np 8 ph.x -in mob2_fonon_06.in > mob2_fonon_06.out
         echo Terminado $directory
         cd ..
+    done
 
 shutdown now
